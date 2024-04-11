4993b6a0a11820f1bf6d7e8fb18fe6f99bb497c0
mmorrow6109 <msmorrow6109@gmail.com>
2024-04-10 20:27:44 -0700

    more starter code added

---
+++ Develop/server/routes/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const path = require('path');
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+// serve up react front-end in production
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html')); //might have to change to 'dist' instead of 'build'
+});
+
+module.exports = router;
