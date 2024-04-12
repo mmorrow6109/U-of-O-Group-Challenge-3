490e37692941b3dfde9bb2596dcddd53a64daca2
mmorrow6109 <msmorrow6109@gmail.com>
2024-04-11 19:14:19 -0700

    main.jsx barebones

--- Develop/client/src/components/main.jsx
+++ Develop/client/src/components/main.jsx
@@ -2,16 +2,10 @@ import Nav from './nav.jsx';
 
 const main = () => {
     return (
-        <div className="bg-indigo-600">
+        <div>
             <Nav/>
-            <div className="border-t border-white"></div> {/* Thin white line */}
-            <Hero/>
-            <div className="border-t border-white"></div>
-            <Portfolio/>
-            <div className="border-t border-white"></div>
-            <Resume/>
-            <Contact/>
-            <Footer/>
         </div>
     )
 };
+
+export default main;
