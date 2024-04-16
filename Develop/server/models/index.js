399c64e66933bab8ae9b78f1dd47699282baa17a
mmorrow6109 <msmorrow6109@gmail.com>
2024-04-15 18:56:05 -0700

    models updated

---
+++ Develop/server/models/index.js
@@ -0,0 +1,7 @@
+const Comment = require('./comment');
+const Dislike = require('./dislike');
+const Like = require('./like');
+const Favorite = require('./favorite');
+const User = require('./user');
+
+module.exports = { Comment, Dislike, Like, Favorite, User };
