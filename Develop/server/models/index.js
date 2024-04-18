6dcb89823b93115deadbf10d1481fa557b7a9bea
mmorrow6109 <msmorrow6109@gmail.com>
2024-04-17 18:33:33 -0700

    like/dislike removed

--- Develop/server/models/index.js
+++ Develop/server/models/index.js
@@ -1,7 +1,5 @@
 const Comment = require('./Comment');
-const Dislike = require('./Dislike');
-const Like = require('./Like');
 const Favorite = require('./Favorite');
 const User = require('./User');
 
-module.exports = { Comment, Dislike, Like, Favorite, User };
+module.exports = { Comment, Favorite, User };
--- Develop/server/routes/api/index.js
+++ Develop/server/routes/api/index.js
@@ -1,13 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./user');
 const commentRoutes = require('./comment');
-const likeRoutes = require('./like');
 const favoriteRoutes = require('./favorite');
 
 // Mount sub-routers under specific base paths
 router.use('/user', userRoutes);
 router.use('/comment', commentRoutes);
-router.use('/like', likeRoutes);
 router.use('/favorite', favoriteRoutes);
 
 module.exports = router;
